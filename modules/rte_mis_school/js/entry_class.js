2adf6ca5ab2998cf2a9f8885737c3581a6be9ce0
jaswinsingh <jaswin.singh@innoraft.com>
2024-03-15 09:49:02 +0530

    RTEMIS-61: Updated code based on review.

--- modules/rte_mis_school/js/entry_class.js
+++ modules/rte_mis_school/js/entry_class.js
@@ -8,20 +8,19 @@
           if ($(this).val() === '_none') {
             $(this).children('option[value!="_none"]').remove();
             // Populate select list with values of selected radio buttons
-            var $wrapper = $('#entry-class-wrapper');
-            var $radioButtons = $('#default-entry-class input[type="radio"]:checked, #optional-entry-class input[type="radio"]:checked');
+            var wrapper = $('#entry-class-wrapper');
+            var radioButtons = $('#default-entry-class input[type="radio"]:checked, #optional-entry-class input[type="radio"]:checked');
             // var radioButtons = $('#default-entry-class input[type="radio"]:checked');
-            $radioButtons.each(function() {
+            radioButtons.each(function() {
               var radioValue = $(this).val();
               var radioLabel = $(this).siblings('label').text();
-              console.log();
               // Add radio button value as an option to the select list
-              $wrapper.find('.entry-class-select').append('<option value="' + radioValue + '">' + radioLabel + '</option>');
+              wrapper.find('.entry-class-select').append('<option value="' + radioValue + '">' + radioLabel + '</option>');
             });
           }
           else {
-            var $select = $(this);
-            $select.children('option:not([value="_none"]):not(:selected)').remove();
+            var select = $(this);
+            select.children('option:not([value="_none"]):not(:selected)').remove();
              // Get the value of the checked radio button from optional-entry-class
             var optionalRadioValue = $('#optional-entry-class input[type="radio"]:checked').val();
             var optionalRadioLabel = $('#optional-entry-class input[type="radio"]:checked + label').text();
@@ -35,8 +34,8 @@
             for (var i = 0; i < radioValues.length; i++) {
               var radioValue = radioValues[i];
               var radioLabel = radioLabels[i];
-              if (radioValue && !$select.find('option[value="' + radioValue + '"]').length) {
-                $select.append('<option value="' + radioValue + '">' + radioLabel + '</option>');
+              if (radioValue && !select.find('option[value="' + radioValue + '"]').length) {
+                select.append('<option value="' + radioValue + '">' + radioLabel + '</option>');
               }
             }
           }
