88768670dcaa06a3655fd33d9ffb726aa3562113
Sandeep Mahlawat <67157-SandeepMahlawat@users.noreply.drupalcode.org>
2024-04-23 14:34:23 +0530

    RTEMIS-128: Main menu theming and structure.

--- modules/rte_mis_gin/js/script.js
+++ modules/rte_mis_gin/js/script.js
@@ -1,8 +1,18 @@
+let $ = jQuery;
 (function ($, Drupal, once) {
     Drupal.behaviors.wrapBlockMenu = {
         attach: function (context, settings) {
             $(once('wrapAll', '.content-wrapper .menu--social-links, .content-wrapper .menu--get-in-touch', context)).wrapAll('<div class="menu-social-links-wrapper block"></div>');
             $(once('wrapAll', '.content-wrapper .block:not(:nth-child(1))',  context)).wrapAll('<div class="footer-wrapper"></div>');
+            $(once('wrapAll', '.region-subheader-wrapper .block-site-logo-section-block, .region-subheader-wrapper .menu--main-menu', context)).wrapAll('<div class="main-menu-wrapper"></div>');
         }
     };
 })(jQuery, Drupal, once);
+
+$(document).ready(function(){
+    $('.menu--main-menu').prepend('<div class="hamburger"><span></span><span></span><span></span></div>');
+    $('.hamburger').click(function(){
+      $(this).toggleClass('active');
+      $('.main-menu-wrapper .menu').toggleClass('collapsed-menu');
+    });
+});
