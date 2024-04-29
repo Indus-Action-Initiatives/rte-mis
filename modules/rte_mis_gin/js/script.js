69cf2e2bd3027f40272ecdca9b988ace7cf26527
Sandeep Mahlawat <67157-SandeepMahlawat@users.noreply.drupalcode.org>
2024-04-29 22:53:17 +0530

    RTEMIS-130: Added the note line in school form.

--- modules/rte_mis_gin/js/script.js
+++ modules/rte_mis_gin/js/script.js
@@ -3,7 +3,8 @@ let $ = jQuery;
     Drupal.behaviors.wrapBlockMenu = {
         attach: function (context, settings) {
             $(once('wrapAll', '.content-wrapper .menu--social-links, .content-wrapper .menu--get-in-touch', context)).wrapAll('<div class="menu-social-links-wrapper block"></div>');
-            $(once('wrapAll', '.content-wrapper .block:not(:nth-child(1))',  context)).wrapAll('<div class="footer-wrapper"></div>');
+            var $footerItems = $(once('wrapAll', '.content-wrapper .footer__site-logo-wrapper, .content-wrapper .menu--quick-links, .content-wrapper .menu--support, .content-wrapper .menu-social-links-wrapper, .content-wrapper .block-site__menu-text-section-block'), context);
+            $footerItems.wrapAll("<div class='footer-wrapper'></div>");
             $(once('wrapAll', '.region-subheader-wrapper .block-site-logo-section-block, .region-subheader-wrapper .menu--main-menu', context)).wrapAll('<div class="main-menu-wrapper"></div>');
         }
     };
@@ -14,4 +15,6 @@ $(document).ready(function(){
       $(this).toggleClass('active');
       $('.main-menu-wrapper .menu').toggleClass('collapsed-menu');
     });
+    var line = "<div class='school-note-wrapper'><p>Note: Email ID and mobile number if entered then both verification is mandatory rewrite</p></div>";
+    $('.js-form-item.form-item.js-form-type-email.form-type--email.js-form-item-mail.form-item--mail').before(line);
 });
