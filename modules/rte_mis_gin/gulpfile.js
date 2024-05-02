193175b177954b0fd6e3e85a261be32b03b3139f
Sandeep Mahlawat <67157-SandeepMahlawat@users.noreply.drupalcode.org>
2024-05-02 15:58:19 +0530

    RTEMIS-141: Js code fixe.

--- modules/rte_mis_gin/gulpfile.js
+++ modules/rte_mis_gin/gulpfile.js
@@ -4,16 +4,16 @@ const rename = require('gulp-rename');
 const glob = require('glob');
 
 gulp.task('scss', function () {
-  return gulp.src('./scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(rename(function (path) {
-      path.basename = path.basename.replace(/\.(scss|sass)$/, '');
-    }))
-    .pipe(gulp.dest('./css'));
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(rename(function (path) {
+            path.basename = path.basename.replace(/\.(scss|sass)$/, '');
+        }))
+        .pipe(gulp.dest('./css'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./scss/**/*.scss', gulp.series('scss'));
+    gulp.watch('./scss/**/*.scss', gulp.series('scss'));
 });
 
 gulp.task('default', gulp.series('scss', 'watch'));
