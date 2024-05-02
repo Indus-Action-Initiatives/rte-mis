aa1665abb473e7e732ed53bee8dce494bac2834e
Sandeep Mahlawat <67157-SandeepMahlawat@users.noreply.drupalcode.org>
2024-05-02 15:19:23 +0530

    RTEMIS-141: Created different libraries for all pages.

--- modules/rte_mis_gin/gulpfile.js
+++ modules/rte_mis_gin/gulpfile.js
@@ -1,18 +1,19 @@
 var gulp = require('gulp');
- const sass = require('gulp-sass')(require('sass')); 
-// var uglifycss = require('gulp-uglifycss');
-gulp.task('scss', function(){ 
-	return gulp.src('./scss/*.scss') 
-.pipe(sass().on('error', sass.logError)) .pipe(gulp.dest('./css')); 
-	});
-gulp.task('css', function(){ 
-	gulp.src('./css/*.css') 
-.pipe(uglifycss({ "uglyComments": true })) .pipe(gulp.dest('./dist')); 
-	});
-	 
-gulp.task('run', gulp.series('scss', 'css'));
-gulp.task('watch', function(){ 
-	gulp.watch('./scss/.scss', ['scss']); 
-	gulp.watch('./css/.css', ['css']); 
+const sass = require('gulp-sass')(require('sass'));
+const rename = require('gulp-rename');
+const glob = require('glob');
+
+gulp.task('scss', function () {
+    return gulp.src('./scss/**/*.scss') // Updated glob pattern
+        .pipe(sass().on('error', sass.logError))
+        .pipe(rename(function (path) {
+            path.basename = path.basename.replace(/\.(scss|sass)$/, ''); // Remove .scss or .sass extension
+        }))
+        .pipe(gulp.dest('./css'));
 });
-gulp.task('default', gulp.series('run', 'watch'));
+
+gulp.task('watch', function () {
+    gulp.watch('./scss/**/*.scss', gulp.series('scss')); // Updated watch task
+});
+
+gulp.task('default', gulp.series('scss', 'watch'));
