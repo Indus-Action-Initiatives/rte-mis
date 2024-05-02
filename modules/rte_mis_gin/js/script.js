e4ef7049a789ee6a72b7118b176374038d7c1bc4
Sandeep Mahlawat <67157-SandeepMahlawat@users.noreply.drupalcode.org>
2024-05-02 15:54:49 +0530

    RTEMIS-141: Js code fixes.

--- modules/rte_mis_gin/js/script.js
+++ modules/rte_mis_gin/js/script.js
@@ -1,15 +1,15 @@
 let $ = jQuery;
 (function ($, Drupal, once) {
-    Drupal.behaviors.wrapBlockMenu = {
-        attach: function (context, settings) {
-            $(once('wrapAll', '.content-wrapper .menu--social-links, .content-wrapper .menu--get-in-touch', context)).wrapAll('<div class="menu-social-links-wrapper block"></div>');
-            var $footerItems = $(once('wrapAll', '.content-wrapper .footer__site-logo-wrapper, .content-wrapper .menu--quick-links, .content-wrapper .menu--support, .content-wrapper .menu-social-links-wrapper, .content-wrapper .block-site__menu-text-section-block'), context);
-            $footerItems.wrapAll("<div class='footer-wrapper'></div>");
-            $(once('wrapAll', '.region-subheader-wrapper .block-site-logo-section-block, .region-subheader-wrapper .menu--main-menu, .region-subheader-wrapper .menu--account', context)).wrapAll('<div class="main-menu-wrapper"></div>');
-            $('.hamburger').click(function(){
-                $(this).toggleClass('active');
-                $('.main-menu-wrapper .menu').toggleClass('collapsed-menu');
-            });
-        }
-    };
+  Drupal.behaviors.wrapBlockMenu = {
+    attach: function (context, settings) {
+      $(once('wrapAll', '.content-wrapper .menu--social-links, .content-wrapper .menu--get-in-touch', context)).wrapAll('<div class="menu-social-links-wrapper block"></div>');
+      var $footerItems = $(once('wrapAll', '.content-wrapper .footer__site-logo-wrapper, .content-wrapper .menu--quick-links, .content-wrapper .menu--support, .content-wrapper .menu-social-links-wrapper, .content-wrapper .block-site__menu-text-section-block'), context);
+      $footerItems.wrapAll("<div class='footer-wrapper'></div>");
+      $(once('wrapAll', '.region-subheader-wrapper .block-site-logo-section-block, .region-subheader-wrapper .menu--main-menu, .region-subheader-wrapper .menu--account', context)).wrapAll('<div class="main-menu-wrapper"></div>');
+      $('.hamburger').click(function () {
+        $(this).toggleClass('active');
+        $('.main-menu-wrapper .menu').toggleClass('collapsed-menu');
+      });
+    }
+  };
 })(jQuery, Drupal, once);
