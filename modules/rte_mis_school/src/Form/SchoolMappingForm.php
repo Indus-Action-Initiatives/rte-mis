327b56c00f28810cc4f6e91bc4e73cb6c685b846
jaswinsingh <jaswin.singh@innoraft.com>
2024-06-10 18:58:00 +0530

    RTEMIS-174: Added transation for cshs element.

--- modules/rte_mis_school/src/Form/SchoolMappingForm.php
+++ modules/rte_mis_school/src/Form/SchoolMappingForm.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\rte_mis_school\Form;
 
+use Drupal\Core\Entity\EntityRepositoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
@@ -35,13 +36,21 @@ class SchoolMappingForm extends FormBase {
   protected $currentUser;
 
   /**
+   * The entity repository.
+   *
+   * @var \Drupal\Core\Entity\EntityRepositoryInterface
+   */
+  protected $entityRepository;
+
+  /**
    * Constructs the service objects.
    *
    * Class constructor.
    */
-  public function __construct(EntityTypeManagerInterface $entityTypeManager, AccountInterface $current_user) {
+  public function __construct(EntityTypeManagerInterface $entityTypeManager, AccountInterface $current_user, EntityRepositoryInterface $entity_repository) {
     $this->entityTypeManager = $entityTypeManager;
     $this->currentUser = $current_user;
+    $this->entityRepository = $entity_repository;
   }
 
   /**
@@ -51,6 +60,7 @@ class SchoolMappingForm extends FormBase {
     return new static(
       $container->get('entity_type.manager'),
       $container->get('current_user'),
+      $container->get('entity.repository')
     );
   }
 
@@ -102,12 +112,14 @@ class SchoolMappingForm extends FormBase {
       // Get the list of options that we will show to the user.
       $options = [];
       foreach ($locationTerms as $term) {
+        $term = $this->entityRepository->getTranslationFromContext($term);
         $options[(int) $term->id()] = new CshsOption($term->label(), (int) $term->parent->target_id == 0 ? NULL : $term->parent->target_id);
       }
 
       $locationParentTerms = $termStorage->loadTree('location_schema', 0, $categorizationDepth, TRUE);
       $labels = [];
       foreach ($locationParentTerms as $term) {
+        $term = $this->entityRepository->getTranslationFromContext($term);
         $labels[] = $term->label();
       }
 
@@ -432,6 +444,7 @@ class SchoolMappingForm extends FormBase {
         // habitation list.
         $child = $term_storage->getChildren($term);
         if (empty($child) && $term_depth[$term->id()] === $max_depth) {
+          $term = $this->entityRepository->getTranslationFromContext($term);
           $options[(int) $term->id()] = $term->label();
         }
       }
@@ -486,6 +499,7 @@ class SchoolMappingForm extends FormBase {
       $term = reset($term);
 
       if ($term instanceof TermInterface) {
+        $term = $this->entityRepository->getTranslationFromContext($term);
         $depth = $schema_term_info[$term->id()]['depth'];
         // Add the current term in the options list.
         $options['labels'][] = $term->label();
@@ -497,6 +511,7 @@ class SchoolMappingForm extends FormBase {
             // habitation list in a separate select list.
             $child = $term_storage->getChildren($term);
             if (!empty($child)) {
+              $term = $this->entityRepository->getTranslationFromContext($term);
               $options['labels'][] = $term->label();
             }
           }
@@ -524,9 +539,11 @@ class SchoolMappingForm extends FormBase {
               // We will have to remove the parent target id for the elements at
               // the categorization depth.
               if ($filteredOption) {
+                $filteredOption = $this->entityRepository->getTranslationFromContext($filteredOption);
                 $options['options'][(int) $filteredOption->id()] = new CshsOption($filteredOption->label());
               }
               elseif ($term_storage->getChildren($term)) {
+                $term = $this->entityRepository->getTranslationFromContext($term);
                 $options['options'][(int) $term->id()] = new CshsOption($term->label(), (int) $term->parent->target_id == 0 ? NULL : $term->parent->target_id);
               }
             }
