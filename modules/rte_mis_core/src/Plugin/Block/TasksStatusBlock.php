469f464da143c5055062521a6ff1a2af6dfa43f9
RohanSingh-inno <rohan.singh@innoraft.com>
2024-06-16 01:19:59 +0530

    RTEMIS-172: Modified the dashboards.

--- modules/rte_mis_core/src/Plugin/Block/TasksStatusBlock.php
+++ modules/rte_mis_core/src/Plugin/Block/TasksStatusBlock.php
@@ -6,7 +6,12 @@ namespace Drupal\rte_mis_core\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Link;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\Core\Url;
+use Drupal\rte_mis_core\Helper\RteCoreHelper;
+use Drupal\views\Views;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -21,6 +26,20 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 final class TasksStatusBlock extends BlockBase implements ContainerFactoryPluginInterface {
 
   /**
+   * The current user service.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * Core helper.
+   *
+   * @var \Drupal\rte_mis_Core\Helper\RteCoreHelper
+   */
+  protected $rteCoreHelper;
+
+  /**
    * Constructs the plugin instance.
    *
    * @param array $configuration
@@ -31,14 +50,22 @@ final class TasksStatusBlock extends BlockBase implements ContainerFactoryPlugin
    *   The plugin implementation definition.
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
    *   The entity type manager service.
+   * @param \Drupal\Core\Session\AccountProxyInterface $currentUser
+   *   The current logged in user.
+   * @param \Drupal\rte_mis_core\Helper\RteCoreHelper $rte_core_helper
+   *   The rte core helper.
    */
   public function __construct(
     array $configuration,
     $plugin_id,
     $plugin_definition,
     private EntityTypeManagerInterface $entityTypeManager,
+    AccountProxyInterface $currentUser,
+    RteCoreHelper $rte_core_helper,
   ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->currentUser = $currentUser;
+    $this->rteCoreHelper = $rte_core_helper;
   }
 
   /**
@@ -50,6 +77,8 @@ final class TasksStatusBlock extends BlockBase implements ContainerFactoryPlugin
       $plugin_id,
       $plugin_definition,
       $container->get('entity_type.manager'),
+      $container->get('current_user'),
+      $container->get('rte_mis_core.core_helper')
     );
   }
 
@@ -57,6 +86,63 @@ final class TasksStatusBlock extends BlockBase implements ContainerFactoryPlugin
    * {@inheritdoc}
    */
   public function build(): array {
+    // Check the roles of the current user.
+    $roles = $this->currentUser->getRoles();
+    // Define the routes for the links.
+    $tasks = [];
+
+    if (array_intersect(['district_admin', 'block_admin'], $roles)) {
+      if ($this->rteCoreHelper->isAcademicSessionValid('school_verification')) {
+        // Load the view programmatically.
+        $view = Views::getView('school_registration');
+        if ($view) {
+          $view->setDisplay('page_1');
+          $view->preExecute();
+          $view->execute();
+          // Check if the view has any results.
+          if (!empty($view->result)) {
+            $tasks['School Approval'] = 'view.school_registration.page_1';
+          }
+        }
+      }
+    }
+    if ($this->rteCoreHelper->isAcademicSessionValid('school_mapping')) {
+      $tasks['Mapping'] = 'rte_mis_school.form.school_mapping';
+    }
+    if (array_intersect(['district_admin', 'block_admin'], $roles)) {
+      if ($this->rteCoreHelper->isAcademicSessionValid('student_verification')) {
+        // Load the view programmatically.
+        $view = Views::getView('student_registration');
+        if ($view) {
+          $view->setDisplay('page_1');
+          $view->preExecute();
+          $view->execute();
+          // Check if the view has any results.
+          if (!empty($view->result)) {
+            $tasks['Student Approval'] = 'view.student_registration.page_1';
+          }
+        }
+      }
+    }
+
+    // Generate the links.
+    $content = [];
+    if ($tasks) {
+      foreach ($tasks as $task_name => $route_name) {
+        $url = Url::fromRoute($route_name);
+        $link = Link::fromTextAndUrl($task_name, $url)->toString();
+        $content[] = [
+          '#markup' => $link,
+        ];
+      }
+    }
+
+    if (empty($content)) {
+      $content[] = [
+        '#markup' => $this->t('No Tasks'),
+      ];
+    }
+
     // Attach the custom library to the block.
     $build = [
       '#theme' => 'tasks_status_block',
@@ -65,7 +151,7 @@ final class TasksStatusBlock extends BlockBase implements ContainerFactoryPlugin
           'rte_mis_core/rte_mis_tasks_status_block',
         ],
       ],
-      '#content' => $this->t('It works!'),
+      '#content' => $content,
     ];
 
     return $build;
