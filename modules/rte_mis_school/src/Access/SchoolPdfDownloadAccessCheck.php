deceed891063329650a171c7f319d2e8fee3d361
RohanSingh-inno <rohan.singh@innoraft.com>
2024-10-05 16:17:50 +0530

    RTEMIS-282: Added the school claim entity pdf print feature.

--- modules/rte_mis_school/src/Access/SchoolPdfDownloadAccessCheck.php
+++ modules/rte_mis_school/src/Access/SchoolPdfDownloadAccessCheck.php
@@ -51,63 +51,68 @@ class SchoolPdfDownloadAccessCheck implements AccessInterface {
     $curr_user_roles = $curr_user->getRoles();
     // Get the mini node from the route.
     $miniNodeId = $routeMatch->getParameters('mini_node')->get('entity_id');
+    $miniNode = $this->entityTypeManager->getStorage('mini_node')->load($miniNodeId);
 
-    // Check if the current user has the required roles.
-    if (array_intersect(['school_admin', 'school'], $curr_user_roles)) {
-      // For school admin/school user roles
-      // Check if the current user's school detail is equal
-      // to current route's mini node.
-      $currUserLinkedMiniNode = $curr_user->get('field_school_details')->getString();
-      if ($currUserLinkedMiniNode == $miniNodeId) {
-        return AccessResult::allowed();
-      }
-      return AccessResult::forbidden();
-    }
-    elseif (array_intersect(['block_admin', 'district_admin'], $curr_user_roles)) {
-      // Logic for district and block admins.
-      // Load the mini node based on miniNodeId.
-      $currUserLinkedLocation = $curr_user->get('field_location_details')->getString();
-      $miniNode = $this->entityTypeManager->getStorage('mini_node')->loadByProperties([
-        'type' => 'school_Details',
-        'id' => $miniNodeId,
-        'status' => 1,
-      ]);
-      $miniNode = reset($miniNode);
-
-      if ($miniNode instanceof EckEntityInterface) {
-        // Get the location details of mini node.
-        $mini_node_location = $miniNode->get('field_location')->getString();
-        // Load all the parents of the mini node's location id.
-        $locationIds = [];
-        if (!empty($mini_node_location)) {
-          $locationTree = $this->entityTypeManager->getStorage('taxonomy_term')->loadAllParents($mini_node_location);
-          foreach ($locationTree as $value) {
-            $locationIds[] = $value->get('tid')->value;
-          }
+    // Below condition is applied for student_detail mini_node.
+    if ($miniNode instanceof EckEntityInterface && $miniNode->bundle() == 'school_details') {
+      // Check if the current user has the required roles.
+      if (array_intersect(['school_admin', 'school'], $curr_user_roles)) {
+        // For school admin/school user roles
+        // Check if the current user's school detail is equal
+        // to current route's mini node.
+        $currUserLinkedMiniNode = $curr_user->get('field_school_details')->getString();
+        if ($currUserLinkedMiniNode == $miniNodeId) {
+          return AccessResult::allowed();
         }
-        if (in_array('block_admin', $curr_user_roles)) {
-          // For Block admin user role,
-          // Check if the mini node's location detail has
-          // the 3rd parent(block) equal to current users location.
-          if (in_array($currUserLinkedLocation, $locationIds)) {
-            return AccessResult::allowed();
-          }
-          return AccessResult::forbidden();
-        }
-        else {
-          // For District admin user role,
-          // Check if the mini node's location detail has
-          // the 4rd parent(block) equal to current users location.
-          if (in_array($currUserLinkedLocation, $locationIds)) {
-            return AccessResult::allowed();
-          }
-          return AccessResult::forbidden();
-        }
-      }
-      else {
         return AccessResult::forbidden();
       }
+      elseif (array_intersect(['block_admin', 'district_admin'], $curr_user_roles)) {
+        // Logic for district and block admins.
+        // Load the mini node based on miniNodeId.
+        $currUserLinkedLocation = $curr_user->get('field_location_details')->getString();
+        $miniNode = $this->entityTypeManager->getStorage('mini_node')->loadByProperties([
+          'type' => 'school_Details',
+          'id' => $miniNodeId,
+          'status' => 1,
+        ]);
+        $miniNode = reset($miniNode);
+
+        if ($miniNode instanceof EckEntityInterface) {
+          // Get the location details of mini node.
+          $mini_node_location = $miniNode->get('field_location')->getString();
+          // Load all the parents of the mini node's location id.
+          $locationIds = [];
+          if (!empty($mini_node_location)) {
+            $locationTree = $this->entityTypeManager->getStorage('taxonomy_term')->loadAllParents($mini_node_location);
+            foreach ($locationTree as $value) {
+              $locationIds[] = $value->get('tid')->value;
+            }
+          }
+          if (in_array('block_admin', $curr_user_roles)) {
+            // For Block admin user role,
+            // Check if the mini node's location detail has
+            // the 3rd parent(block) equal to current users location.
+            if (in_array($currUserLinkedLocation, $locationIds)) {
+              return AccessResult::allowed();
+            }
+            return AccessResult::forbidden();
+          }
+          else {
+            // For District admin user role,
+            // Check if the mini node's location detail has
+            // the 4rd parent(block) equal to current users location.
+            if (in_array($currUserLinkedLocation, $locationIds)) {
+              return AccessResult::allowed();
+            }
+            return AccessResult::forbidden();
+          }
+        }
+        else {
+          return AccessResult::forbidden();
+        }
+      }
     }
+
     return AccessResult::allowed();
   }
 
