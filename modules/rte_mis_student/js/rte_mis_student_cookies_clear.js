f91d05064064e315a97d4ee2f96830d4f7865a3d
RohanSingh-inno <rohan.singh@innoraft.com>
2024-12-05 18:29:57 +0530

    RTEMIS-540: Improve student application flow.
    [1] Make the logout button visible once a student has logged in.
    [2] If a user presses the back button in the browser then the user should be logged out (after a warning).
    [3] The Navigation of the public pages should not be visible once a student has logged in.
    [4] The Save button should be renamed to Next.
    [5] On the last screen, the Save button should be visible
    [6] Along with Next, the Previous button should also be there. Next and Previous should navigate across the tabs (accordions).
    [7] Add a Cancel button to cancel student application.
    [8] Breadcrumbs are not correct and they should be hyperlinks.
    [9] Alter the student pdf file name.

---
+++ modules/rte_mis_student/js/rte_mis_student_cookies_clear.js
@@ -0,0 +1,59 @@
+(function ($, Drupal, once) {
+  Drupal.behaviors.studentClearToken = {
+    attach: function (context, settings) {
+      once('studentClearToken', 'body', context).forEach(function () {
+        // Function to get the value of a specific cookie
+        function getCookie(name) {
+          const value = `; ${document.cookie}`;
+          const parts = value.split(`; ${name}=`);
+          if (parts.length === 2) return parts.pop().split(';').shift();
+        }
+
+        // Function to extract 'code' from a URL query string or destination parameter
+        function extractCodeFromUrl(url) {
+          const urlParams = new URLSearchParams(url);
+          if (urlParams.has('code')) {
+            return urlParams.get('code');
+          }
+          if (urlParams.has('destination')) {
+            const decodedDestination = decodeURIComponent(urlParams.get('destination'));
+            if (decodedDestination.includes('?')) {
+              const queryString = decodedDestination.split('?')[1];
+              const destinationParams = new URLSearchParams(queryString);
+      
+              // Extract and return the 'code' parameter
+              const code = destinationParams.get('code');
+              return code;
+            }
+          }
+          return null;
+        }
+
+        const tokenCookie = getCookie('student-token');
+        
+        if (tokenCookie) {
+          // Extract the code from the current URL
+          const code = extractCodeFromUrl(window.location.search);
+          if (code) {
+            // Push an initial state into the browser history.
+            history.pushState(null, document.title, window.location.href);
+            window.addEventListener('popstate', function () {
+              const userConfirmed = confirm(
+                "Are you sure you want to go back? You will be logged out if 'Ok' is pressed."
+              );
+
+              if (userConfirmed) {
+                // Expire the 'student-token' cookie
+                Cookies.remove('student-token', { path: '/' });
+                location.reload();
+              } else {
+                history.pushState(null, document.title, window.location.href);
+              }
+            });
+          }
+        }
+      });
+    },
+  };
+
+})(jQuery, Drupal, once);
