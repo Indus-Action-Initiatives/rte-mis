1a8e5a4fc217a266879f7d290ada7efbe55c15a2
Akisargar <sargar.akshay96@gmail.com>
2026-02-05 12:43:13 +0530

    RTE-40: Improve Location Details UX by making District and Block read-only

--- modules/rte_mis_school/js/cshs_validation.js
+++ modules/rte_mis_school/js/cshs_validation.js
@@ -4,6 +4,19 @@
       $(document).ready(function() {
         // Disable 1st and 2nd level of cshs select list.
         $('.school-details-cshs div[data-level="0"] select, .school-details-cshs div[data-level="1"] select').prop('disabled', true);
+
+        once('cshs-validation', '.school-details-cshs select', context).forEach(function (element) {
+          var $element = $(element);
+          var level = $element.closest('div[data-level]').data('level');
+
+          if (level === 0 || level === 1) {
+            var selectedText = $element.find('option:selected').text();
+            var $readonlyDiv = $('<span>').addClass('readonly-text').text(selectedText).css({ 'padding': '8px', });
+            $readonlyDiv.css({ 'display': 'inline-block'});
+            $element.after($readonlyDiv); 
+            $element.hide();
+          }
+        })
       })
     }
   };
