869a8d5dc4b4fd9ebd9d3aeb2b0885c5ba1e8f2b
Akisargar <sargar.akshay96@gmail.com>
2026-03-23 14:30:13 +0530

    RTE-71: Implement homepage hero banner using SDC and Storybook

--- modules/rte_mis_home/src/Form/BannerSettings.php
+++ modules/rte_mis_home/src/Form/BannerSettings.php
@@ -13,7 +13,8 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 /**
  * Configure rte_mis_home settings for this site.
  */
-final class BannerSettings extends ConfigFormBase {
+final class BannerSettings extends ConfigFormBase
+{
 
   /**
    * The file URL generator service.
@@ -32,7 +33,8 @@ final class BannerSettings extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container) {
+  public static function create(ContainerInterface $container)
+  {
     return new static(
       $container->get('config.factory'),
       $container->get('file_url_generator'),
@@ -50,7 +52,8 @@ final class BannerSettings extends ConfigFormBase {
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager service.
    */
-  public function __construct($config_factory, FileUrlGeneratorInterface $file_url_generator, EntityTypeManagerInterface $entity_type_manager) {
+  public function __construct($config_factory, FileUrlGeneratorInterface $file_url_generator, EntityTypeManagerInterface $entity_type_manager)
+  {
     parent::__construct($config_factory);
     $this->fileUrlGenerator = $file_url_generator;
     $this->entityTypeManager = $entity_type_manager;
@@ -59,21 +62,24 @@ final class BannerSettings extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  public function getFormId(): string {
+  public function getFormId(): string
+  {
     return 'rte_mis_home_banner_settings';
   }
 
   /**
    * {@inheritdoc}
    */
-  protected function getEditableConfigNames(): array {
+  protected function getEditableConfigNames(): array
+  {
     return ['rte_mis_home.settings'];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state): array {
+  public function buildForm(array $form, FormStateInterface $form_state): array
+  {
     $config = $this->config('rte_mis_home.settings');
 
     $banner_image_fid = $config->get('banner_image') ?: NULL;
@@ -107,13 +113,90 @@ final class BannerSettings extends ConfigFormBase {
       ];
     }
 
+    $content = $config->get('banner_content');
+
+    $form['banner_content'] = [
+      '#type' => 'text_format',
+      '#title' => $this->t('Content'),
+      '#format' => $content['format'] ?? 'full_html',
+      '#allowed_formats' => ['full_html'],
+      '#default_value' => $content['value'] ?? '',
+    ];
+
+    $links = $config->get('links') ?: [];
+    $num_links = $form_state->get('num_links');
+    if ($num_links === NULL) {
+      $num_links = max(1, count($links));
+      $form_state->set('num_links', $num_links);
+    }
+
+    $form['links_wrapper'] = [
+      '#type' => 'container',
+      '#attributes' => ['id' => 'links-wrapper'],
+    ];
+
+    $form['links_wrapper']['links'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Banner Links'),
+      '#tree' => TRUE,
+    ];
+
+    for ($i = 0; $i < $num_links; $i++) {
+      $form['links_wrapper']['links'][$i] = [
+        '#type' => 'details',
+        '#title' => $this->t('Link @num', ['@num' => $i + 1]),
+        '#open' => TRUE,
+      ];
+      $form['links_wrapper']['links'][$i]['title'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Link Title'),
+        '#default_value' => $links[$i]['title'] ?? '',
+      ];
+      $form['links_wrapper']['links'][$i]['url'] = [
+        '#type' => 'textfield',
+        '#title' => $this->t('Link URL'),
+        '#description' => $this->t('Example: /node/1 or https://example.com'),
+        '#default_value' => $links[$i]['url'] ?? '',
+      ];
+    }
+
+    $form['links_wrapper']['add_link'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Add another link'),
+      '#submit' => ['::addLinkSubmit'],
+      '#ajax' => [
+        'callback' => '::addLinkCallback',
+        'wrapper' => 'links-wrapper',
+      ],
+      '#limit_validation_errors' => [],
+    ];
+
     return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * Submit handler for the "Add another link" button.
+   */
+  public function addLinkSubmit(array &$form, FormStateInterface $form_state)
+  {
+    $num_links = $form_state->get('num_links') + 1;
+    $form_state->set('num_links', $num_links);
+    $form_state->setRebuild();
+  }
+
+  /**
+   * AJAX callback for the "Add another link" button.
+   */
+  public function addLinkCallback(array &$form, FormStateInterface $form_state)
+  {
+    return $form['links_wrapper'];
   }
 
   /**
    * {@inheritdoc}
    */
-  public function submitForm(array &$form, FormStateInterface $form_state): void {
+  public function submitForm(array &$form, FormStateInterface $form_state): void
+  {
     $config = $this->configFactory->getEditable('rte_mis_home.settings');
 
     // Handle the file upload.
@@ -131,6 +214,17 @@ final class BannerSettings extends ConfigFormBase {
     }
 
     $config->set('banner_image', $saved_file_id);
+    $config->set('banner_content', $form_state->getValue('banner_content'));
+
+    $links = $form_state->getValue('links') ?: [];
+    $valid_links = [];
+    foreach ($links as $link) {
+      if (!empty($link['title']) && !empty($link['url'])) {
+        $valid_links[] = $link;
+      }
+    }
+    $config->set('links', $valid_links);
+
     $config->save();
 
     parent::submitForm($form, $form_state);
